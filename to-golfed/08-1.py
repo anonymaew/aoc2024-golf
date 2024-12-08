9962ef9852ff1b15d539a50eeb31fe82fd6aa3c3
Napat Srichan <mail@napatsc.com>
2024-12-07 22:27:23 -0800

    add day 8 sol

---
+++ to-golfed/08-1.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from itertools import combinations
+
+table = []
+antennas = defaultdict(list)
+antinodes = set()
+
+for i,l in enumerate(open(0).readlines()):
+    table += [l]
+    for j,c in enumerate(l[:-1]):
+        if c!='.':
+            antennas[c] += [(i,j)]
+
+w=len(table)
+for k in antennas:
+    l = antennas[k]
+    if (len(l)<2):
+        continue
+    for pair in combinations(l,2):
+        diff = (pair[1][0]-pair[0][0],pair[1][1]-pair[0][1])
+        ant1 = (pair[0][0]-diff[0],pair[0][1]-diff[1])
+        ant2 = (pair[1][0]+diff[0],pair[1][1]+diff[1])
+        if 0<=ant1[0]<w and 0<=ant1[1]<w:
+            antinodes |= set([ant1[0]+ant1[1]*w])
+        if 0<=ant2[0]<w and 0<=ant2[1]<w:
+            antinodes |= set([ant2[0]+ant2[1]*w])
+
+print(len(antinodes))
